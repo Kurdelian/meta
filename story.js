e6ae04ede7a768f89042b098c5501372a4ee857b
Olha Kurdelian <89872697+Kurdelian@users.noreply.github.com>
2022-09-04 17:57:25 +0300

    storu

--- story.js
+++ story.js
@@ -1,8 +1,9 @@
 const founders = document.querySelector('#founders');
-console.log(founders)
 
 const storyBoxPart = document.querySelector('.story__box_part');
-console.log(storyBoxPart)
+
+const partners = document.querySelector('#partners')
+console.log(partners)
 
 founders.onmouseover = function(event) {
     const founders = event.founders;
@@ -13,6 +14,16 @@ founders.onmouseout = function(event) {
     const founders = event.founders;
     storyBoxPart.style.opacity= '0';
   };
+
+// partners.onmouseover = function(event) {
+//     const partners = event.partners;
+//     storyBoxPart.style.opacity = '1';
+//   };
+
+// partners.onmouseout = function(event) {
+//     const partners = event.partners;
+//     storyBoxPart.style.opacity= '0';
+//   };
 // founders.addEventListener('click', getFounders ())
 
 // function getFounders () {
